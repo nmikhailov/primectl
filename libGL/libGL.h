ce64189085823f751432f7caa7e4f2f43d180a39
Nikita Mikhaylov <nslqqq@gmail.com>
2013-01-29 18:50:31 +0700

    include fix

--- libGL/libGL.h
+++ libGL/libGL.h
@@ -1,7 +1,7 @@
 #ifndef LIBGL_H_
 #define LIBGL_H_
 
-#include "../common/config.h"
+#include "common/config.h"
 
 void __attribute__ ((constructor)) on_load(void);
 void __attribute__ ((destructor)) on_unload(void);
